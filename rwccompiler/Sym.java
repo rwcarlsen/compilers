f90ad402aa9edd536413b45ac6e463871a9a8430
Robert Carlsen <rwcarlsen@gmail.com>
2012-01-25 08:44:44 -0600

    initial commit.

---
+++ rwccompiler/Sym.java
@@ -0,0 +1,23 @@
+
+
+package rwccompiler;
+
+public class Sym {
+
+  public Sym(String type) {
+
+  }
+
+  public String getType() {
+    return "blank-type";
+  }
+
+  public String toString() {
+    return "blank-string";
+  }
+
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
+
