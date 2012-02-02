e09043c8de9568260d51ff6182970132088f35ab
Robert Carlsen <rwcarlsen@gmail.com>
2012-02-02 10:38:53 -0600

    added javadoc comments to rwccompiler classes.

--- src/rwccompiler/Sym.java
+++ src/rwccompiler/Sym.java
@@ -1,18 +1,22 @@
 
 package rwccompiler;
 
+/** Symbol class used in compiler symbol table. */
 public class Sym {
 
   private String type;
 
+  /** Initialize Sym with specified type. */
   public Sym(String type) {
     this.type = type;
   }
 
+  /** Get this symbol's type. */
   public String getType() {
     return this.type;
   }
 
+  /** Get nicely formatted string representing the symbol. */
   public String toString() {
     return this.type;
   }
--- src/rwccompiler/SymTab.java
+++ src/rwccompiler/SymTab.java
@@ -3,10 +3,12 @@ package rwccompiler;
 
 import java.util.*;
 
+/** Compiler symbol table */
 public class SymTab {
 
   private List<Map<String, Sym>> table;
 
+  /** initialize symbol table with one blank Map */
   public SymTab() {
     this.table = new LinkedList<Map<String, Sym>>();
 
@@ -14,6 +16,10 @@ public class SymTab {
     this.table.add(newMap);
   }
 
+  /** Remove (delete) the HashMap from the front of the list.
+   *
+   * @throws EmptySymTabException the SymTab's list is empty
+   */
   public void removeMap() throws EmptySymTabException {
     if (this.table.size() == 0) {
       throw new EmptySymTabException();
@@ -22,11 +28,18 @@ public class SymTab {
     this.table.remove(0);
   }
 
+  /** Add a new, empty HashMap to the front of the list. */
   public void addMap() {
     Map<String, Sym> newMap = new HashMap<String, Sym>();
     this.table.add(0, newMap);
   }
 
+  /** Add the given name and sym to the first HashMap in the list.
+   *
+   * @throws EmptySymTabException the SymTab's list is empty
+   * @throws DuplicateException the first Hashmap in the list already
+   *         contains the given name as a key
+   */
   public void insert(String name, Sym sym)
       throws DuplicateException, EmptySymTabException {
 
@@ -43,6 +56,13 @@ public class SymTab {
     map.put(name, sym);
   }
 
+  /** Retrieve the Sym for the given name from the first hashmap in the list.
+   *
+   *
+   * @return If the first HashMap in the list has name as a key, return
+   *         the associated Sym. If this SymTab's list is empty or if the
+   *         key is not present in the first Map, return null.
+   */
   public Sym localLookup(String name) {
     if (this.table.size() == 0) {
       return null;
@@ -55,6 +75,15 @@ public class SymTab {
     return map.get(name);
   }
 
+  /** Retrieve the Sym from the first hashmap that contains the given
+   *  name as a key.
+   *
+   *
+   * @return If a HashMap in the list has name as a key, return
+   *         the associated Sym from the first HashMap that contains it.
+   *         If this SymTab's list is empty or if the key is not present
+   *         in any Map, return null.
+   */
   public Sym globalLookup(String name) {
     if (this.table.size() == 0) {
       return null;
@@ -68,12 +97,12 @@ public class SymTab {
     return null;
   }
 
+  /** pretty-print the symbol table */
   public void print() {
     System.out.print("\nSYMBOL TABLE\n");
     for (Map<String, Sym> map : this.table) {
       System.out.println(map.toString());
     }
   }
-
 }
 
